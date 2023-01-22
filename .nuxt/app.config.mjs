6d6ff77f8ce3225bdec7671e93b5a2f322ce640c
edward_brightwolves <edward.van.vlasselaer@brightwolves.eu>
2023-01-22 19:35:31 +0100

    preview

---
+++ .nuxt/app.config.mjs
@@ -0,0 +1,8 @@
+
+import { defuFn } from '/Users/edward/projects/digitmint/demo/demo/node_modules/.pnpm/defu@6.1.1/node_modules/defu/dist/defu.mjs'
+
+const inlineConfig = {}
+
+
+
+export default defuFn(inlineConfig)
---
+++ .output/public/_payload.js
@@ -0,0 +1,1 @@
+export default {data:{},prerenderedAt:1674412461275}
